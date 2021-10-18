2891f2b35eab9721ff1f28434a50b42f0150cf3b
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-10-18 14:19:11 +0200

    Ajout de la classe Salle

--- src/main/java/fr/hexagone/back/Salle.java
+++ src/main/java/fr/hexagone/back/Salle.java
@@ -1,6 +1,25 @@
 package fr.hexagone.back;
 
+import java.util.ArrayList;
+
 public class Salle {
+
+    private String nomSalle;
+    private int nbPlace;
+    private boolean projecteur = false;
+    private ArrayList<Reservation> reservations = new ArrayList<>();
+
+    public Salle(String nomSalle, int nbPlace, boolean projecteur){
+        this.nomSalle = nomSalle;
+        this.nbPlace = nbPlace;
+        this.projecteur = projecteur;
+    }
+    public Salle(String nomSalle, int nbPlace, boolean projecteur, ArrayList<Reservation> reservations){
+        this.nomSalle = nomSalle;
+        this.nbPlace = nbPlace;
+        this.projecteur = projecteur;
+        this.reservations = reservations;
+    }
 
 
 }
