ec821c0a7b2c9dade319c44d9a51e38928097dcd
anthony <anthony_pro1998@outlook.fr>
2021-10-18 16:37:23 +0200

    créer le contrôlleur de réservation.

--- src/main/java/fr/hexagone/back/ReservationController.java
+++ src/main/java/fr/hexagone/back/ReservationController.java
@@ -2,8 +2,15 @@ package fr.hexagone.back;
 
 import fr.hexagone.dao.ReservationRepository;
 import fr.hexagone.model.Reservation;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import fr.hexagone.model.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 public class ReservationController {
@@ -11,8 +18,27 @@ public class ReservationController {
     @Autowired
     ReservationRepository reservationRepository;
 
+    @PostConstruct
     public void init(){
+        if(reservationRepository.count() == 0){
+            reservationRepository.save(new Reservation("first@email.fr", LocalDateTime.now(), 2));
+            reservationRepository.save(new Reservation("first@email.fr", LocalDateTime.of(
+                    LocalDateTime.now().getYear(), LocalDateTime.now().getMonth(),LocalDateTime.now().getDayOfMonth(),
+                    15,0
+            ), 2));
+            reservationRepository.save(new Reservation("first@email.fr", LocalDateTime.of(
+                    LocalDateTime.now().getYear(), LocalDateTime.now().getMonth(),LocalDateTime.now().getDayOfMonth(),
+                    12,0
+            ), 2));
+        }
+
 
     }
+
+    public List<Reservation> findAllReservations(){
+        return reservationRepository.findAll();
+    }
+
+    // String email, LocalDateTime startDateTime, int duration
 
 }
