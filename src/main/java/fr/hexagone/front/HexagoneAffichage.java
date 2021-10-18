02e73bb08385c0f45cb3cc28b896589e9918ac82
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-10-18 14:30:02 +0200

    Ajout de la classe HexagoneAffichage

---
+++ src/main/java/fr/hexagone/front/HexagoneAffichage.java
@@ -0,0 +1,23 @@
+package fr.hexagone.front;
+
+import fr.hexagone.back.Salle;
+import javafx.scene.shape.Path;
+
+import java.util.ArrayList;
+
+public class HexagoneAffichage {
+
+    private Path forme;
+    private ArrayList<SalleAffichage> sallesAffichages;
+
+
+    public HexagoneAffichage(Path forme, ArrayList<SalleAffichage> sallesAffichages){
+        this.forme = forme;
+        this.sallesAffichages = sallesAffichages;
+    }
+
+
+
+
+
+}
