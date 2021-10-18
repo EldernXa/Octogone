8d08a445d2fa7ebd240d29670aefbd649a75af2d
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-10-18 14:15:58 +0200

    Ajout de la classe Reservation

--- src/main/java/fr/hexagone/back/Reservation.java
+++ src/main/java/fr/hexagone/back/Reservation.java
@@ -1,4 +1,18 @@
 package fr.hexagone.back;
 
 public class Reservation {
+
+    private static int ID = 0;
+    private Date hDebut;
+    private int duree;
+    private String mail;
+
+    public Reservation(Date hDebut, int duree, String mail){
+        this.hDebut = hDebut;
+        this.duree = duree;
+        this.mail = mail;
+    }
+
+
+
 }
