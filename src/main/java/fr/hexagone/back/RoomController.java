0b53992aeece422d756ca96a102285a4ba6c527b
sykom1 <saber.tnifas@etu.univ-amu.fr>
2021-10-18 16:18:44 +0200

    suppression application.properties et amélioration des controllers (création des méthodes findAll et findByName)

--- src/main/java/fr/hexagone/back/RoomController.java
+++ src/main/java/fr/hexagone/back/RoomController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Controller
 public class RoomController {
@@ -16,17 +17,28 @@ public class RoomController {
 
     @PostConstruct
     public void init(){
-        roomRepository.save(new Room("H1",1));
-        roomRepository.save(new Room("H2",2,"Tableau"));
-        roomRepository.save(new Room("H3",3,"Video Projecteur","Tableau"));
-        roomRepository.save(new Room("H4",4,"Video Projecteur","Tableau"));
-        roomRepository.save(new Room("O4",4,"Video Projecteur","Tableau","Table Ronde"));
-        roomRepository.save(new Room("O8",8,"Video Projecteur","Tableau","Table Ronde"));
-        roomRepository.save(new Room("P1",1,"Tableau"));
-        roomRepository.save(new Room("P2",2,"Tableau"));
-        roomRepository.save(new Room("F2",2,"Video Projecteur","Tableau"));
-        roomRepository.save(new Room("F3",3,"Video Projecteur","Tableau"));
+        if(roomRepository.count() == 0){
+            roomRepository.save(new Room("H1",1));
+            roomRepository.save(new Room("H2",2,"Tableau"));
+            roomRepository.save(new Room("H3",3,"Video Projecteur","Tableau"));
+            roomRepository.save(new Room("H4",4,"Video Projecteur","Tableau"));
+            roomRepository.save(new Room("O4",4,"Video Projecteur","Tableau","Table Ronde"));
+            roomRepository.save(new Room("O8",8,"Video Projecteur","Tableau","Table Ronde"));
+            roomRepository.save(new Room("P1",1,"Tableau"));
+            roomRepository.save(new Room("P2",2,"Tableau"));
+            roomRepository.save(new Room("F2",2,"Video Projecteur","Tableau"));
+            roomRepository.save(new Room("F3",3,"Video Projecteur","Tableau"));
+        }
 
+        System.out.println(getRoom("H4").getName());
+    }
+
+    public List<Room> findAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    public Room getRoom(String name){
+        return roomRepository.findByName(name);
     }
 
 
--- src/main/java/fr/hexagone/dao/RoomRepository.java
+++ src/main/java/fr/hexagone/dao/RoomRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+    public Room findByName(String name);
 }
