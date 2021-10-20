15cbfa2e7f8a69f2b57cf526d6443b5629864cab
anthony <anthony_pro1998@outlook.fr>
2021-10-20 08:38:19 +0200

    Suppression de commentaire inutile dans la classe HexagoneController.

--- src/main/java/fr/hexagone/back/HexagoneController.java
+++ src/main/java/fr/hexagone/back/HexagoneController.java
@@ -36,35 +36,17 @@ public class HexagoneController {
             Availability available = null;
             List<Reservation> listReservation = getListReservationOfARoom(room);
             for(Reservation reservation : listReservation){
-                /*LocalDateTime localDateTime = LocalDateTime.of(reservation.getStartDateTime().getYear(),
-                        reservation.getStartDateTime().getMonth(), reservation.getStartDateTime().getDayOfMonth(),
-                        reservation.getStartDateTime().getHour()+reservation.getDuration(),
-                        reservation.getStartDateTime().getMinute());*/
                 LocalDateTime localDateTime = reservationController.getTimeAfterDuration(reservation);
 
-/*                LocalDateTime localDateTimeWithDuration = LocalDateTime.of(date.getYear(), date.getMonth(),
-                        date.getDayOfMonth(), date.getHour()+duration, date.getMinute());*/
                 LocalDateTime localDateTimeWithDuration = reservationController.getTimeAfterDuration(date, duration);
-
-                /*if(((date.isAfter(reservation.getStartDateTime()) && date.isBefore(localDateTime)) ||
-                        date.isEqual(reservation.getStartDateTime()) ||
-                        date.isEqual(localDateTime)) &&
-                        ((localDateTimeWithDuration.isAfter(reservation.getStartDateTime())&&localDateTimeWithDuration.isAfter(localDateTime))
-                        || localDateTimeWithDuration.isEqual(reservation.getStartDateTime()) || localDateTimeWithDuration.isEqual(localDateTime))){
-                    available = Availability.NOT_YET;
-                }*/
 
                 if (isOverlapping(reservation.getStartDateTime(), localDateTime, date, localDateTimeWithDuration)) {
                     available = Availability.NOT_YET;
                 }
 
-
-
-
                 if(available != null){
                     break;
                 }
-
             }
 
             if(available == null){
