b506431b247081065aaaa9f28362b6319fb98491
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-10-20 23:26:53 +0200

    BUg partie Reservation Contoller

--- src/main/java/fr/hexagone/back/HexagoneController.java
+++ src/main/java/fr/hexagone/back/HexagoneController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HexagoneController {
+public class  HexagoneController {
 
     @Autowired
     ReservationRepository reservationRepository;
