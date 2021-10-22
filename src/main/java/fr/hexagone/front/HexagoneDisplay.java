b5c972cba1741e5229997b13d973e0e55afb21cf
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-10-22 18:59:56 +0200

    Fin de l'implémentation de toutes les salles

--- src/main/java/fr/hexagone/front/HexagoneDisplay.java
+++ src/main/java/fr/hexagone/front/HexagoneDisplay.java
@@ -16,6 +16,11 @@ public class HexagoneDisplay {
     private final Polygon room3;
     private final Polygon room4;
     private final Polygon room5;
+    private final Polygon room6;
+    private final Polygon room7;
+    private final Polygon room8;
+    private final Polygon room9;
+    private final Polygon room10;
 
     private ArrayList<RoomDisplay> roomDisplays = new ArrayList<>();
 
@@ -114,6 +119,76 @@ public class HexagoneDisplay {
         this.room5.setOnMouseExited(mouseEvent -> room5.setFill(Color.WHITE));
         this.roomDisplays.add(new RoomDisplay(new Coordinate(x,y),this.room5,null,Color.WHITE));
 
+        this.room6 = new Polygon();
+
+        room6.getPoints().addAll(
+                Math.sqrt(3)/2*150-2.5,-225.,
+                (Math.sqrt(3)/2*150+2.5)/2,-225.,
+                (Math.sqrt(3)/2*150+2.5)/2,-150.,
+                Math.sqrt(3)/2*300-2.5,-150.
+        );
+        this.room6.setFill(Color.WHITE);
+        this.room6.setStroke(Color.BLACK);
+        this.room6.setOnMouseEntered(mouseEvent -> room6.setFill(Color.RED));
+        this.room6.setOnMouseExited(mouseEvent -> room6.setFill(Color.WHITE));
+        this.roomDisplays.add(new RoomDisplay(new Coordinate(x,y),this.room6,null,Color.WHITE));
+
+        this.room7 = new Polygon();
+
+        room7.getPoints().addAll(
+                Math.sqrt(3)/2*300-2.5,-150.,
+                150.,-150.,
+                150.,0.,
+                Math.sqrt(3)/2*300-2.5,0.
+
+        );
+        this.room7.setFill(Color.WHITE);
+        this.room7.setStroke(Color.BLACK);
+        this.room7.setOnMouseEntered(mouseEvent -> room7.setFill(Color.RED));
+        this.room7.setOnMouseExited(mouseEvent -> room7.setFill(Color.WHITE));
+        this.roomDisplays.add(new RoomDisplay(new Coordinate(x,y),this.room7,null,Color.WHITE));
+
+        this.room8 = new Polygon();
+
+        room8.getPoints().addAll(
+                Math.sqrt(3)/2*300-2.5,0.,
+                150.,0.,
+                150.,150.,
+                Math.sqrt(3)/2*300-2.5,150.
+        );
+        this.room8.setFill(Color.WHITE);
+        this.room8.setStroke(Color.BLACK);
+        this.room8.setOnMouseEntered(mouseEvent -> room8.setFill(Color.RED));
+        this.room8.setOnMouseExited(mouseEvent -> room8.setFill(Color.WHITE));
+        this.roomDisplays.add(new RoomDisplay(new Coordinate(x,y),this.room8,null,Color.WHITE));
+
+        this.room9 = new Polygon();
+
+        room9.getPoints().addAll(
+                -Math.sqrt(3)/2*150+2.5,225.,
+                -(Math.sqrt(3)/2*150-2.5)/2,225.,
+                -(Math.sqrt(3)/2*150-2.5)/2,150.,
+                -Math.sqrt(3)/2*300+2.5,150.
+        );
+        this.room9.setFill(Color.WHITE);
+        this.room9.setStroke(Color.BLACK);
+        this.room9.setOnMouseEntered(mouseEvent -> room9.setFill(Color.RED));
+        this.room9.setOnMouseExited(mouseEvent -> room9.setFill(Color.WHITE));
+        this.roomDisplays.add(new RoomDisplay(new Coordinate(x,y),this.room9,null,Color.WHITE));
+
+        this.room10 = new Polygon();
+
+        room10.getPoints().addAll(
+                Math.sqrt(3)/2*150-2.5,225.,
+                (Math.sqrt(3)/2*150+2.5)/2,225.,
+                (Math.sqrt(3)/2*150+2.5)/2,150.,
+                Math.sqrt(3)/2*300-2.5,150.
+        );
+        this.room10.setFill(Color.WHITE);
+        this.room10.setStroke(Color.BLACK);
+        this.room10.setOnMouseEntered(mouseEvent -> room10.setFill(Color.RED));
+        this.room10.setOnMouseExited(mouseEvent -> room10.setFill(Color.WHITE));
+        this.roomDisplays.add(new RoomDisplay(new Coordinate(x,y),this.room10,null,Color.WHITE));
 
     }
 
