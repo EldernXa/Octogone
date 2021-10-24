9374f32b248a896be58cf0bfafcc318cf7f9bc74
anthony <anthony_pro1998@outlook.fr>
2021-10-24 11:38:11 +0200

    Réalisation de Javadoc pour la classe HexagoneController pour la documentation.

--- src/main/java/fr/hexagone/back/HexagoneController.java
+++ src/main/java/fr/hexagone/back/HexagoneController.java
@@ -13,9 +13,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @Controller
 public class  HexagoneController {
 
+    /**
+     * Variable pour indiquer à partir de quel durée une salle est bientôt disponible.
+     */
     private static final int DURATION_FOR_SOON = 1;
 
     @Autowired
@@ -24,10 +28,22 @@ public class  HexagoneController {
     @Autowired
     RoomController roomController;
 
+    /**
+     * Pour obtenir une liste de réservation à partir d'une salle.
+     * @param room une salle
+     * @return une liste contenant toutes les réservations d'une salle.
+     */
     public List<Reservation> getListReservationOfARoom(Room room){
         return reservationRepository.getAllReservationFromRoom(room);
     }
 
+    /**
+     * Renvoie la liste des disponibilités des salles en fonction de plusieurs paramètres.
+     * @param date la date de la réservations voulues.
+     * @param duration la durée de la réservation.
+     * @param capacity la capacité de la salle voulue.
+     * @return une map contenant en clé une salle et en valeur la disponibilité de cette salle.
+     */
     public Map<Room, Availability> listAvailabilityRoom(LocalDateTime date, int duration, int capacity){
         Map<Room, Availability> listRoomSorted = new HashMap<>();
 
@@ -40,6 +56,14 @@ public class  HexagoneController {
         return listRoomSorted;
     }
 
+    /**
+     *
+     * @param roomToVerify la salle pour laquel on veut vérifier la disponibilité.
+     * @param capacity la capacité que l'utilisateur désire avoir.
+     * @param date la date de la réservation.
+     * @param duration la durée de réservation que l'utilisateur désire.
+     * @return la disponibilité de la salle donné en paramètre.
+     */
     private Availability getAvailabilityForARoom(Room roomToVerify, int capacity, LocalDateTime date, int duration){
         Availability available = null;
         if(roomToVerify.getCapacity() >= capacity){
@@ -64,6 +88,13 @@ public class  HexagoneController {
         return available;
     }
 
+    /**
+     *
+     * @param reservation une réservation pour vérifier qu'elle rentre pas en conflit avec la réservation voulue par l'utilisateur.
+     * @param date la date de la réservation voulue par l'utilisateur.
+     * @param duration la durée que l'utilisateur désire.
+     * @return la disponibilité en fonction de si la réservation actuelle rentre en conflit avec la réservation voulue par l'utilisateur.
+     */
     private Availability getAvailabilityFromAReservation(Reservation reservation, LocalDateTime date, int duration){
         Availability available = null;
         LocalDateTime resEndDateTime = reservation.getEndDateTime();
